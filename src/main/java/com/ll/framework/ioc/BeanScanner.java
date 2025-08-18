cb4194eb915156884ec26a6ff5b8922a7b80397f
AEU <aeuplayground@gmail.com>
2025-08-18 18:39:00 +0900

    [Refactor] addTypeMapper 메소드 추가

--- src/main/java/com/ll/framework/ioc/BeanScanner.java
+++ src/main/java/com/ll/framework/ioc/BeanScanner.java
@@ -38,36 +38,40 @@ public class BeanScanner {
         for (Class<?> clazz : classes) {
             String className = Ut.str.lcfirst(clazz.getSimpleName());
             recipes.put(className, clazz);
-            typeMapper.putIfAbsent(clazz.getTypeName(), new ArrayList<>());
-            typeMapper.get(clazz.getTypeName()).add(className);
-
+            addTypeMapper(clazz.getTypeName(), className);
+            
+            // 해당 클래스에 Configuration 어노테이션까지 붙어있을 경우 Bean 어노테이션 메소드도 레시피 등록
             if (clazz.isAnnotationPresent(Configuration.class)) {
                 Method[] methods = clazz.getDeclaredMethods();
                 for (Method method : methods) {
                     String methodName = Ut.str.lcfirst(method.getName());
                     if (!method.isAnnotationPresent(Bean.class)) continue;
                     recipes.put(methodName, method);
-                    typeMapper.putIfAbsent(method.getReturnType().getTypeName(), new ArrayList<>());
-                    typeMapper.get(method.getReturnType().getTypeName()).add(methodName);
+                    addTypeMapper(method.getReturnType().getTypeName(), methodName);
                 }
             }
         }
     }
 
-    public List<String> getFieldNameList(Class<?> clazz) throws Exception {
+    public void addTypeMapper(String keyName, String classOrMethodName) {
+        typeMapper.putIfAbsent(keyName, new ArrayList<>());
+        typeMapper.get(keyName).add(classOrMethodName);
+    }
+
+    public List<String> getFieldNameList(Class<?> clazz) {
         List<String> FieldList = new ArrayList<>(){};
 
         Field[] fields = clazz.getDeclaredFields();
         for (Field field : fields) {
             field.setAccessible(true);
-            if (!recipes.containsKey(field.getName())) continue; // 예외 복구하기
+            if (!recipes.containsKey(field.getName())) continue;
             FieldList.add(field.getName());
         }
 
         return FieldList;
     }
 
-    public List<Class<?>> getFieldClassList(List<String> fieldNameList) throws Exception {
+    public List<Class<?>> getFieldClassList(List<String> fieldNameList) {
         List<Class<?>> classList = new ArrayList<>(){};
         for (String name : fieldNameList) {
             if (!recipes.containsKey(name)) continue;
@@ -77,14 +81,14 @@ public class BeanScanner {
         return classList;
     }
 
-    public List<String> getParameterNameList(Method method) throws Exception {
+    public List<String> getParameterNameList(Method method) {
         List<String> fieldList = new ArrayList<>(){};
         Parameter[] parameters = method.getParameters();
         for(Parameter parameter : parameters){
             String typeName = parameter.getType().getTypeName();
-            fieldList.add(typeMapper.get(typeName).getFirst());
-        }
 
+            if (typeMapper.containsKey(typeName)) fieldList.add(typeMapper.get(typeName).getFirst());
+        }
         return fieldList;
     }
 
@@ -104,7 +108,6 @@ public class BeanScanner {
 
             clazz.getDeclaredConstructor(fieldClassArray).setAccessible(true);
             bean = clazz.getDeclaredConstructor(fieldClassArray).newInstance(obs);
-            beans.put(beanName, bean);
         } else if (what instanceof Method method) {
             List<String> paramNameList = getParameterNameList(method);
             String parentClassName = method.getDeclaringClass().getSimpleName();
@@ -112,11 +115,11 @@ public class BeanScanner {
             Object[] obs = paramNameList.stream().map(this::genBean).toArray();
 
             bean = method.invoke(genBean(Ut.str.lcfirst(parentClassName)), obs);
-            beans.put(beanName, bean);
-            return bean;
+        } else {
+            throw new RuntimeException("recipes에 Class나 Method가 아닌 값이 있습니다.");
         }
 
-
+        beans.put(beanName, bean);
         return bean;
     }
 }
